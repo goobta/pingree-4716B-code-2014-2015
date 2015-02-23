3a68e528559ad4bfdd4e62ac5648f92e6db4c95c
agupta231 <rukna1000@gmail.com>
2015-02-23 12:43:50 -0500

    Moving all of the existing code

---
+++ 12POINTSBABY.c
@@ -0,0 +1,76 @@
+#pragma config(Motor,  port2,           driveRight,    tmotorVex393, openLoop, reversed)
+#pragma config(Motor,  port3,           driveLeft,     tmotorVex393, openLoop)
+#pragma config(Motor,  port4,           builder,       tmotorVex393, openLoop)
+#pragma config(Motor,  port5,           cube,          tmotorVex393, openLoop)
+#pragma config(Motor,  port6,           liftLeftTop,   tmotorVex393, openLoop, reversed)
+#pragma config(Motor,  port7,           liftLeftBack,  tmotorVex393, openLoop, reversed)
+#pragma config(Motor,  port8,           liftRightTop,  tmotorVex393, openLoop)
+#pragma config(Motor,  port9,           liftRightBack, tmotorVex393, openLoop)
+
+void drive(int power) {
+	motor[driveLeft] = power;
+	motor[driveRight] = power;
+}
+
+void driveTrainLeft(int power) {
+	motor[driveLeft] = power;
+}
+
+
+void driveTrainRight(int power) {
+	motor[driveRight] = power;
+}
+
+void lift(int power) {
+	motor[liftLeftBack] = power;
+	motor[liftLeftTop] = power;
+	motor[liftRightTop] = power;
+	motor[liftRightBack] = power;
+}
+
+void builderClaw(int power) {
+	motor[builder] = power;
+}
+
+void cubeClaw(int power) {
+	motor[cube] = power;
+}
+
+task main()
+{
+	driveTrainLeft(127);
+	driveTrainRight(-127);
+	builderClaw(127);
+	wait1Msec(500);
+
+	driveTrainLeft(127);
+	driveTrainRight(-127);
+	builderClaw(0);
+	wait1Msec(250);
+
+	drive(-127);
+	wait1Msec(250);
+
+	builderClaw(-127);
+	drive(0);
+	wait1Msec(250);
+
+	builderClaw(0);
+	drive(-127);
+	wait1Msec(300);
+
+	drive(0);
+	driveTrainRight(-127);
+	driveTrainLeft(127);
+	wait1Msec(100);
+
+	drive(127);
+	wait1Msec(500);
+
+	drive(0);
+	builderClaw(-127);
+	wait1Msec(250);
+
+	builderClaw(0);
+	wait1Msec(10);
+}
---
+++ README.txt
@@ -0,0 +1,21 @@
+Bonjour Scott and Alex.
+
+The raw user driving code is the aW6Raw.c file BUT THE FILE THAT YOU HAVE TO INSTALL ON THE 
+ROBOT IS COMPETITIONCODE.C.
+
+the competition code is fairly straightforward void autonomous() is the autonomus code and usercontrol is obviously usercontrol.
+
+for the autonomous code, it should be pretty straight forward, the robot moves back at full speed for half a second. To load the autonomous, place a cube on the square, and the robot will just move away from it, scoring a point. make sure position the robot in such a way that i doesn't hit other robots.
+
+THE SKYRISE BUILDER MOTOR MUST BE PLUGED INTO PORT 4
+THE CUBE CLAW MUST BE PLUGGED INTO PORT 5
+
+Reverse the motors if nessacary. 
+
+sorry for the typos, SO TIRED
+text me with questions, and I might be able to program (I'm supposed to be somewhere, but there is a high chance that I will have access to my laptop)
+
+LET'S GET THE W
+-Ankur
+
+-You the man!
