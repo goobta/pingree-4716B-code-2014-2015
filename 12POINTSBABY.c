72443831b4c773d7f269ca32b153d5ea9fa8d08a
agupta231 <rukna1000@gmail.com>
2015-02-23 12:46:51 -0500

    commented out code robot doesn't mess up when testing autonomous

--- 12POINTSBABY.c
+++ 12POINTSBABY.c
@@ -43,34 +43,34 @@ task main()
 	builderClaw(127);
 	wait1Msec(500);
 
-	driveTrainLeft(127);
-	driveTrainRight(-127);
-	builderClaw(0);
-	wait1Msec(250);
+	// driveTrainLeft(127);
+	// driveTrainRight(-127);
+	// builderClaw(0);
+	// wait1Msec(250);
 
-	drive(-127);
-	wait1Msec(250);
+	// drive(-127);
+	// wait1Msec(250);
 
-	builderClaw(-127);
-	drive(0);
-	wait1Msec(250);
+	// builderClaw(-127);
+	// drive(0);
+	// wait1Msec(250);
 
-	builderClaw(0);
-	drive(-127);
-	wait1Msec(300);
+	// builderClaw(0);
+	// drive(-127);
+	// wait1Msec(300);
 
-	drive(0);
-	driveTrainRight(-127);
-	driveTrainLeft(127);
-	wait1Msec(100);
+	// drive(0);
+	// driveTrainRight(-127);
+	// driveTrainLeft(127);
+	// wait1Msec(100);
 
-	drive(127);
-	wait1Msec(500);
+	// drive(127);
+	// wait1Msec(500);
 
-	drive(0);
-	builderClaw(-127);
-	wait1Msec(250);
+	// drive(0);
+	// builderClaw(-127);
+	// wait1Msec(250);
 
-	builderClaw(0);
-	wait1Msec(10);
+	// builderClaw(0);
+	// wait1Msec(10);
 }
